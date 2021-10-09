ad113b2861688b8e486ebf33d4470d36e5434e68
Thiago Ferreira <thiagocristhian@gmail.com>
2021-10-08 21:39:18 -0300

    Requisito 5

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -26,4 +26,9 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        result = instance.search(position)
+        print(result)
+        return sys.stdout.write(f"{result}")
+    except IndexError:
+        return sys.stderr.write("Posição inválida")
