15e38471b52f920e69e7a793ee05b80f3ee5e638
Thiago Ferreira <thiagocristhian@gmail.com>
2021-10-10 17:59:22 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,4 +20,24 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    for index_file in range(len(instance)):
+        get_file = instance.search(index_file)
+        add_or_no = False
+        lines = list()
+        for index_line, line in enumerate(get_file['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                add_or_no = True
+                lines.append({
+                    'linha': index_line + 1,
+                    'conteudo': line,
+                })
+
+        if add_or_no:
+            result.append({
+                'palavra': word,
+                'arquivo': get_file['nome_do_arquivo'],
+                'ocorrencias': lines
+            })
+
+    return result
